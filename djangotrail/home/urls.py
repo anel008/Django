e92b61e01a2260df38a63d9d6f7c107cd52072f3
anel babu <anelbabu008@gmail.com>
2024-03-08 03:43:14 +0530

    templates added

--- djangotrail/home/urls.py
+++ djangotrail/home/urls.py
@@ -2,4 +2,8 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.index),
+    path('about', views.about),
+    path('contact', views.contact),
+    path('doctor', views.doctor),
+    path('booking', views.booking),
 ]
--- djangotrail/home/views.py
+++ djangotrail/home/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+def about(request):
+    return render(request, 'about.html')
+def contact(request):
+    return render(request, 'contact.html')
+def doctor(request):
+    return render(request, 'doctor.html')
+def booking(request):
+    return render(request, 'booking.html')

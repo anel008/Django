66081651007c3829f8a763bf53c28f1ddddf8aa2
anel babu <anelbabu008@gmail.com>
2024-03-09 14:58:35 +0530

    department fixed with backend

--- djangotrail/home/views.py
+++ djangotrail/home/views.py
@@ -1,23 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Department
 
 # Create your views here.
 def index(request):
-    person = {
-        'Name' : 'Anel babu',
-        'age' : 23,
-
-    }
-    return render(request, 'index.html',person)
+    return render(request, 'index.html')
 def about(request):
-    number = {'num1': 1}
-
-    return render(request, 'about.html',number)
+    return render(request, 'about.html')
 def contact(request):
+
     return render(request, 'contact.html')
 def doctor(request):
+
     return render(request, 'doctor.html')
 def booking(request):
+
     return render(request, 'booking.html')
 def department(request):
-    return render(request,'department.html')
+
+    depp= Department.objects.all()
+    x = {
+        'dep':depp
+        }
+
+    return render(request,'department.html',x)
